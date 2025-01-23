469f6ca6e750c394f8bee745983c1c1b7d6c68cd
Niclas Siegfried <niclas.siegfried@hak-kitz.ac.at>
2025-01-23 14:55:43 +0100

    added FormCreateTable, added CreateColumnPanel

---
+++ Mitarbeitsprojekt/FormCreateTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mitarbeitsprojekt
+{
+    public partial class FormCreateTable : Form
+    {
+        public FormCreateTable()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCreateTable_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
